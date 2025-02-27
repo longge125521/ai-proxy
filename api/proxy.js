516eb5b84f361a14ee107a03317eb6a8f5ddfe31
魏锦龙 <wb-wjl613335@alibaba-inc.com>
2025-02-27 15:17:51 +0800

    测试

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://api.groq.com/openai";//your website url
+  let target = "https://api.groq.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
